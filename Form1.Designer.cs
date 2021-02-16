e7ab3bf723116a6a56a9d7a3efa1923e594a29a6
Paul Moore <bossbossbosscow@gmail.com>
2021-02-16 15:10:41 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace Part_1_3_Chapter_Assignment
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tkbrHeight = new System.Windows.Forms.TrackBar();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.nudAge = new System.Windows.Forms.NumericUpDown();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tkbrHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAge)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tkbrHeight
+            // 
+            this.tkbrHeight.Location = new System.Drawing.Point(148, 30);
+            this.tkbrHeight.Maximum = 251;
+            this.tkbrHeight.Minimum = 55;
+            this.tkbrHeight.Name = "tkbrHeight";
+            this.tkbrHeight.Size = new System.Drawing.Size(238, 56);
+            this.tkbrHeight.TabIndex = 0;
+            this.tkbrHeight.Value = 55;
+            this.tkbrHeight.ValueChanged += new System.EventHandler(this.tkbrHeight_ValueChanged);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(161, 191);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(225, 22);
+            this.txtName.TabIndex = 1;
+            // 
+            // nudAge
+            // 
+            this.nudAge.Location = new System.Drawing.Point(161, 120);
+            this.nudAge.Maximum = new decimal(new int[] {
+            120,
+            0,
+            0,
+            0});
+            this.nudAge.Name = "nudAge";
+            this.nudAge.Size = new System.Drawing.Size(225, 22);
+            this.nudAge.TabIndex = 2;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.Red;
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(430, 30);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(216, 193);
+            this.btnSubmit.TabIndex = 3;
+            this.btnSubmit.Text = "Execute Program";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.BackColor = System.Drawing.Color.Aqua;
+            this.lblHeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeight.Location = new System.Drawing.Point(3, 40);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(79, 25);
+            this.lblHeight.TabIndex = 4;
+            this.lblHeight.Text = "Height: ";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.BackColor = System.Drawing.Color.Aqua;
+            this.lblAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAge.Location = new System.Drawing.Point(21, 120);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(54, 25);
+            this.lblAge.TabIndex = 5;
+            this.lblAge.Text = "Age:";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.Aqua;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(5, 188);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(70, 25);
+            this.lblName.TabIndex = 6;
+            this.lblName.Text = "Name:";
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.BackColor = System.Drawing.Color.Lime;
+            this.lblMessage.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(47, 279);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(588, 121);
+            this.lblMessage.TabIndex = 7;
+            this.lblMessage.Text = "[Message will appear here]";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(702, 418);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.nudAge);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.tkbrHeight);
+            this.Name = "Main";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.tkbrHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAge)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar tkbrHeight;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.NumericUpDown nudAge;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Part_1_3_Chapter_Assignment
+{
+    public partial class Main : Form
+    {
+        int height = 0;
+        string name = "";
+        decimal age = 0;
+        string lifeExpectancyMsg = "";
+        string heightDifferenceMsg = "";
+
+        public Main()
+        {
+            InitializeComponent();         
+        }
+
+        private void tkbrHeight_ValueChanged(object sender, EventArgs e)
+        {
+            height = tkbrHeight.Value;
+            lblHeight.Text = $"Height: {height / 100.0}m";
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            height = tkbrHeight.Value;
+            name = txtName.Text;
+            age = nudAge.Value;
+            
+            if (age >= 82)
+                lifeExpectancyMsg = $"you have already surpassed the average life expectancy in Canada by {age - 82} years";
+            else
+                lifeExpectancyMsg = $"you have {82 - age} years until you reach the average life expectancy in Canada";
+            
+            if (height == 251)
+                heightDifferenceMsg = "you are the same height as the tallest person alive at 2.51m";
+            else
+                heightDifferenceMsg = $"you are {(251 - height) / 100.0}m shorter than the tallest person alive";
+
+            lblMessage.Text = $"Hello {name}, {lifeExpectancyMsg}, and {heightDifferenceMsg}.";
+            tkbrHeight.Enabled = false;
+            nudAge.Enabled = false;
+            txtName.Enabled = false;
+            btnSubmit.Enabled = false;
+        }
+    }
+}
